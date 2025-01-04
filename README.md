ec9b2ae5c05ee656d1a09476c10a26623276e391
-PT_AxH_RV- <0@a>
2025-01-04 22:02:11 +0800

    添加了生成移动汉字的动图的功能。

--- README.md
+++ README.md
@@ -1,43 +1,48 @@
 # fontify
 
 ## 简介
+
 `fontify.py` 是一个 Python 脚本，用于将图片转换为汉字图像。该脚本通过分析输入图片的灰度值，将其转换为相应的汉字，生成一幅新的图像。它支持多种字体和亮度、对比度的调整，适合用于艺术创作和图像处理。
 
 ## 特性
-- 将图片转换为汉字图像；
-- 支持自定义字体；
-- 可调整图像的亮度和对比度（即预处理）以得到更好的效果；
-- 支持预处理后的图像保存；
-- 可自定义汉字的数量和大小。
+
+- 将图片转换为汉字图像
+- 支持自定义字体
+- 可调整图像的亮度和对比度（即预处理）以得到更好的效果
+- 支持预处理后的图像保存
+- 可自定义汉字的数量和大小
 
 ## 准备
+
 
 1. 确保您的系统已安装 `Python 3.x`；
 
 2. 确保您的系统已安装以下Python第三方库：
-   - `pillow`；
-   - `tqdm`；
-   - `numpy`.
-
-   使用命令：
-
-   ```bash
-   pip install pillow tqdm numpy
-   ```
-
-   若您在安装numpy时遇到错误，请尝试：
-
-   ```bash
-   apt install python-numpy
-   ```
+  - `pillow`；
+  - `tqdm`；
+  - `numpy`；
+  - `sympy`.
+  
+  使用命令：
+  
+  ```bash
+  pip install pillow tqdm numpy sympy
+  ```
+  
+  若您在安装numpy时遇到错误，请尝试：
+  
+  ```bash
+  apt install python-numpy
+  ```
 
 3. 克隆此仓库：
-   ```bash
-   git clone https://github.com/tile-WWDCCS-name/fontify
-   cd fontify
-   ```
+  ```bash
+  git clone https://github.com/tile-WWDCCS-name/fontify
+  cd fontify
+  ```
 
 ## 使用方法
+
 运行脚本时，需要指定输入图片路径和输出图片路径。可以使用以下命令行参数：
 
 ```bash
@@ -47,21 +52,57 @@ python fontify.py <image_path> <output_path> [options]
 ### 参数说明
 - `image_path`: 输入图片的路径；
 - `output_path`: 输出图片的路径；
+- `-bgc`, `--background_color`: 背景颜色，默认为 #FFFFFF；
 - `-psp`, `--path_save_pretreated`: 预处理后的图片的保存路径；
 - `-nbt`, `--normalize_brightness_target`: 标准化图片亮度目标，范围在 0~255 之间；
 - `-cf`, `--contrast_factor`: 对比度因子，默认为 1.0；
-- `-c`, `--count`: 分配给同一灰度的汉字数量，默认为 5；
 - `-s`, `--size`: 横向汉字个数，默认为 300；
 - `-cs`, `--char_size`: 单个汉字的尺寸，默认为 12；
-- `-crf`, `--colorful`: 输出彩色图片；
-- `-fp`, `--font_path`: 字体路径，默认为脚本所在文件夹下的 `font.ttf`；
+- `-crf`, `--colorful`: 输出彩色图片（与`--text_color`参数互斥）；
+- `-tc`, `--text_color`: 文字颜色，默认为 #FFFFFF（与`--colorful`参数互斥）；
+- `-fp`, `--font_path`: 基本和扩A的字体路径，默认为脚本所在文件夹下的 `font.ttf`；
 - `-fp2`, `--font_path2`: 扩B~扩F的字体路径，默认为脚本所在文件夹下的 `font2.ttf`；
 - `-fp3`, `--font_path3`: 扩G~扩H的字体路径，默认为脚本所在文件夹下的 `font3.ttf`；
 - `-fp4`, `--font_path4`: 扩I的字体路径，默认为脚本所在文件夹下的 `font4.ttf`。
 
+### 子命令
+- `static`, `s`: 生成静态图片；
+  - `-c`, `--count`: 分配给同一灰度的汉字数量，默认为 5。
+- `mobile`, `m`: 生成动态图片（GIF）；
+  - `-fps`, `--fps`: 帧率，默认为 10；
+  - `-t`, `--time`: 生成 GIF 的时长，默认为 3；
+  - `-t_min`, `--t_minimum`: t 的取值下限，默认为 0；
+  - `-t_max`, `--t_maximum`: t 的取值上限，默认为 1；
+  - `-pex`, `--parametric_equation_x`: 控制汉字偏移量的参数方程的 x 坐标表达式，默认值为 t；
+  - `-pey`, `--parametric_equation_y`: 控制汉字偏移量的参数方程的 y 坐标表达式，默认值为 t。
+> [!NOTE]
+>
+> 如果要生成动态图片，则为使生成的图片在循环播放时不会出现明显的撕裂感，需要让参数方程的开始坐标的横坐标与结束坐标的横坐标模一同余，纵坐标与纵坐标模一同余。（模一同余，即一个数与另一个数的小数部分相同，如 1.27 和 7.27）
+> 
+> 如在上述操作后仍有撕裂感，请尝试延长时间或提高帧率。
+
 ### 示例
 ```bash
-python fontify.py input.jpg output.png -nbt 200 -cf 1.5 -c 10 -s 400 -cs 14
+python fontify.py input.jpg output.png -nbt 200 -cf 1.5 -s 400 -cs 14 static -c 10
+```
+
+向右下角移动（即参数方程`(t, t)` `(0≤t≤1)`的路径）：
+```bash
+python fontify.py input.jpg output.gif -nbt 200 -cf 1.5 -s 400 -cs 14 mobile -fps 15 -t 5
+```
+
+按正弦函数的路径移动（即参数方程`(t, sin(2πt))` `(0≤t≤1)`的路径）：
+```bash
+python fontify.py input.jpg output.gif -nbt 200 -cf 1.5 -s 400 -cs 14 m -pex "t" -pey "sin(2*pi*t)"
+```
+
+按圆的路径移动（即参数方程`(sin(t), cos(t))` `(0≤t≤2π)`的路径）：
+```bash
+python fontify.py input.jpg output.gif -nbt 200 -cf 1.5 -s 400 -cs 14 m -pex "sin(t)" -pey "cos(t)" -t_max 6.28318530718
+```
+按反正弦函数的路径移动（即参数方程`(3t, 6/π × arcsin(t))` `(-1≤t≤1)`的路径）：
+```bash
+python fontify.py input.jpg output.gif -nbt 200 -cf 1.5 -s 400 -cs 14 m -pex "3*t" -pey "6/pi * asin(t)" -t_min -1 -t 10
 ```
 
 ## chinese_character_blackness_sort.py
@@ -76,10 +117,10 @@ python chinese_character_blackness_sort.py [options]
 ```
 
 ### 参数说明
-- `-fp`, `--font_path`: 基本和扩A的字体路径，默认为脚本所在文件夹下的 `font.ttf`（可选）
-- `-fp2`, `--font_path2`: 扩B~扩F的字体路径，默认为脚本所在文件夹下的 `font2.ttf`（可选）
-- `-fp3`, `--font_path3`: 扩G~扩H的字体路径，默认为脚本所在文件夹下的 `font3.ttf`（可选）
-- `-fp4`, `--font_path4`: 扩I的字体路径，默认为脚本所在文件夹下的 `font4.ttf`（可选）
+- `-fp`, `--font_path`: `基本区和扩A`的字体路径，默认为脚本所在文件夹下的 `font.ttf`（可选）
+- `-fp2`, `--font_path2`: `扩B~扩F`的字体路径，默认为脚本所在文件夹下的 `font2.ttf`（可选）
+- `-fp3`, `--font_path3`: `扩G~扩H`的字体路径，默认为脚本所在文件夹下的 `font3.ttf`（可选）
+- `-fp4`, `--font_path4`: `扩I`的字体路径，默认为脚本所在文件夹下的 `font4.ttf`（可选）
 
 ### 示例
 ```bash
@@ -87,7 +128,8 @@ python chinese_character_blackness_sort.py -fp path/to/font.ttf -fp2 path/to/fon
 ```
 
 ## 注意事项
-- 确保输入的图片路径正确且图片格式受支持；
-- 字体文件（如 `font.ttf`, `font2.ttf`, `font3.ttf`, `font4.ttf`）需放在与脚本相同的目录下，或者提供正确的路径；
-- 对于较大的 `--size` 或 `--char_size` 参数值，处理时间可能会较长；
-- 若您需使用其他的自定义字体，请确保您的字体能够显示对应区段的所有汉字，并在运行 `fontify.py` 前运行 `chinese_character_blackness_sort.py`（注意传入您需要的字体路径）；
+
+- 确保输入的图片路径正确且图片格式受支持
+- 字体文件（如 `font.ttf`, `font2.ttf`, `font3.ttf`, `font4.ttf`）需放在与脚本相同的目录下，或者提供正确的路径
+- 对于较大的 `--size` 或 `--char_size` 参数值，处理时间可能会较长
+- 若您需使用其他的自定义字体，请确保您的字体能够显示对应区段的所有汉字，并在运行 `fontify.py` 前运行 `chinese_character_blackness_sort.py`（注意传入您需要的字体路径）。
--- fontify.py
+++ fontify.py
@@ -6,39 +6,152 @@ import argparse
 from PIL import Image, ImageDraw, ImageFont, ImageEnhance
 from tqdm import tqdm
 import numpy as np
+import sympy as sp
+
+cur_dir = dirname(__file__)
+
+
+def hex_to_rgb(hex_value):
+    hex_value = hex_value.strip("#")
+
+    red = int(hex_value[0:2], 16)
+    green = int(hex_value[2:4], 16)
+    blue = int(hex_value[4:6], 16)
+
+    return red, green, blue
+
 
 parser = argparse.ArgumentParser(description="这是一个能将图片转成汉字的脚本")
-parser.add_argument('image_path', type=str,
-                    help='输入图片的路径')
-parser.add_argument('output_path', type=str,
-                    help='输出图片的路径')
-parser.add_argument('-psp', '--path_save_pretreated', type=str,
-                    help='预处理后的图片的保存路径，不填时不保存')
-parser.add_argument('-nbt', '--normalize_brightness_target', type=int,
-                    help='标准化图片亮度目标，在0~255之间，值越大图片越亮，不填时不处理')
-parser.add_argument('-cf', '--contrast_factor', type=float, default=1.0,
-                    help='对比度因子，在0以上，大于1时提高对比度，小于1时降低对比度，为1时无效，默认为1')
-parser.add_argument('-c', '--count', type=int, default=5,
-                    help='分配给同一灰度的汉字数量，默认为5，最大100，建议在1~15之间')
-parser.add_argument('-s', '--size', type=int, default=300,
-                    help='横向汉字个数，默认为300（不建议超过500）')
-parser.add_argument('-cs', '--char_size', type=int, default=12,
-                    help='单个汉字的尺寸，单位为像素，默认为12（建议在8~16之间）')
-parser.add_argument('-crf', '--colorful', action='store_true',
-                    help='输出彩色图片')
-parser.add_argument('-fp', '--font_path', type=str, default=join((cur_dir := dirname(__file__)), 'font.ttf'),
-                    help='字体路径，默认为此脚本所在的文件夹下的font.ttf文件，支持otf，但用otf会让速度变慢')
-parser.add_argument('-fp2', '--font_path2', type=str, default=join((cur_dir := dirname(__file__)), 'font2.ttf'),
-                    help='扩B~扩F的字体路径，默认为此脚本所在的文件夹下的font2.ttf文件，支持otf，但用otf会让速度变慢')
-parser.add_argument('-fp3', '--font_path3', type=str, default=join((cur_dir := dirname(__file__)), 'font3.ttf'),
-                    help='扩G~扩H的字体路径，默认为此脚本所在的文件夹下的font3.ttf文件，支持otf，但用otf会让速度变慢')
-parser.add_argument('-fp4', '--font_path4', type=str, default=join((cur_dir := dirname(__file__)), 'font4.ttf'),
-                    help='扩I的字体路径，默认为此脚本所在的文件夹下的font4.ttf文件，支持otf，但用otf会让速度变慢')
+parser.add_argument("image_path", type=str, help="输入图片的路径")
+parser.add_argument("output_path", type=str, help="输出图片的路径")
+parser.add_argument(
+    "-bgc",
+    "--background_color",
+    type=str,
+    default="#FFFFFF",
+    help="背景颜色，默认纯白（#FFFFFF)",
+)
+parser.add_argument(
+    "-psp",
+    "--path_save_pretreated",
+    type=str,
+    help="预处理后的图片的保存路径，不填时不保存",
+)
+parser.add_argument(
+    "-nbt",
+    "--normalize_brightness_target",
+    type=int,
+    help="标准化图片亮度目标，在 0~255 之间，值越大图片越亮，不填时不处理",
+)
+parser.add_argument(
+    "-cf",
+    "--contrast_factor",
+    type=float,
+    default=1.0,
+    help="对比度因子，在 0 以上，大于 1 时提高对比度，小于 1 时降低对比度，为 1 时无效，默认为 1",
+)
+parser.add_argument(
+    "-s",
+    "--size",
+    type=int,
+    default=300,
+    help="横向汉字个数，默认为 300（不建议超过 500）",
+)
+parser.add_argument(
+    "-cs",
+    "--char_size",
+    type=int,
+    default=12,
+    help="单个汉字的尺寸，单位为像素，默认为 12（建议在 8~16 之间）",
+)
+
+colorful_group = parser.add_mutually_exclusive_group()
+colorful_group.add_argument(
+    "-crf", "--colorful", action="store_true", help="输出彩色图片"
+)
+colorful_group.add_argument(
+    "-tc",
+    "--text_color",
+    type=str,
+    default="#FFFFFF",
+    help="文字颜色，默认纯黑（#000000)",
+)
+
+parser.add_argument(
+    "-fp",
+    "--font_path",
+    type=str,
+    default=join(cur_dir, "font.ttf"),
+    help="扩 A 和基本区字体路径，默认为此脚本所在的文件夹下的 font.ttf 文件，支持 otf，但用 otf 会让速度变慢",
+)
+parser.add_argument(
+    "-fp2",
+    "--font_path2",
+    type=str,
+    default=join(cur_dir, "font2.ttf"),
+    help="扩 B~扩 F 字体路径，默认为此脚本所在的文件夹下的 font2.ttf 文件，支持 otf，但用 otf 会让速度变慢",
+)
+parser.add_argument(
+    "-fp3",
+    "--font_path3",
+    type=str,
+    default=join(cur_dir, "font3.ttf"),
+    help="扩 G~扩 H 字体路径，默认为此脚本所在的文件夹下的 font3.ttf 文件，支持 otf，但用 otf 会让速度变慢",
+)
+parser.add_argument(
+    "-fp4",
+    "--font_path4",
+    type=str,
+    default=join(cur_dir, "font4.ttf"),
+    help="扩 I 字体路径，默认为此脚本所在的文件夹下的 font4.ttf 文件，支持 otf，但用 otf 会让速度变慢",
+)
+
+subparsers = parser.add_subparsers(
+    help="控制生成的图片中的汉字是否移动", required=True, dest="command"
+)
+static_parser = subparsers.add_parser(
+    "static", help="生成汉字不移动的图片", aliases=["s"]
+)
+static_parser.add_argument(
+    "-c",
+    "--count",
+    type=int,
+    default=5,
+    help="分配给同一灰度的汉字数量，默认为 5，最大 100，建议在 1~15 之间",
+)
+mobile_parser = subparsers.add_parser(
+    "mobile", help="生成汉字移动的图片", aliases=["m"]
+)
+mobile_parser.add_argument(
+    "-fps", "--fps", type=int, default=10, help="帧率，默认为 10，建议在 3~30 之间"
+)
+mobile_parser.add_argument(
+    "-t", "--time", type=int, default=3, help="生成 gif 的时长，默认为 3"
+)
+mobile_parser.add_argument(
+    "-t_min", "--t_minimum", type=float, default=0, help="t 的取值下限，默认为 0"
+)
+mobile_parser.add_argument(
+    "-t_max", "--t_maximum", type=float, default=1, help="t 的取值上限，默认为 1"
+)
+mobile_parser.add_argument(
+    "-pex",
+    "--parametric_equation_x",
+    type=str,
+    default="t",
+    help="控制汉字偏移量的参数方程的 x 坐标表达式，默认值为 t，参变数为 t",
+)
+mobile_parser.add_argument(
+    "-pey",
+    "--parametric_equation_y",
+    type=str,
+    default="t",
+    help="控制汉字偏移量的参数方程的 y 坐标表达式，默认值为 t，参变数为 t",
+)
 args = parser.parse_args()
 
 image_path = args.image_path
 output_path = args.output_path
-count = args.count
 size = args.size
 char_size = args.char_size
 font_path = args.font_path
@@ -47,60 +160,184 @@ font_path3 = args.font_path3
 font_path4 = args.font_path4
 normalize_brightness_target = args.normalize_brightness_target
 path_save_pretreated = args.path_save_pretreated
+colorful = args.colorful
+contrast_factor = args.contrast_factor
+background_color = hex_to_rgb(args.background_color)
+text_color = hex_to_rgb(args.text_color)
 
-dict_ = tuple(map(lambda i: i[:count], load(open(join(cur_dir, 'data.json')))))
-img = Image.open(image_path)
-
-width, height = img.size
-aspect_ratio = height / width
-img = img.resize((size, (height := round(size * aspect_ratio))))
-
-if args.colorful:
-    img_rgba = img.convert('RGBA')
-    img_data = np.array(img_rgba)
-    bgc_r = round(255 - np.mean(img_data[:, :, 0]))
-    bgc_g = round(255 - np.mean(img_data[:, :, 1]))
-    bgc_b = round(255 - np.mean(img_data[:, :, 2]))
-    bgc = (round(bgc_r * 299/1000 + bgc_g * 587/1000 + bgc_b * 114/1000), ) * 3 + (255, )
-else:
-    bgc = (255, 255, 255, 255)
-
-enhancer = ImageEnhance.Contrast(img)
-img = enhancer.enhance(args.contrast_factor)
-if normalize_brightness_target is not None:
-    from normalize_brightness import normalize_brightness
-    img = normalize_brightness(img, normalize_brightness_target)
-
-if path_save_pretreated is not None:
-    img.save(path_save_pretreated)
-
-img = img.convert('L')
-string_array = np.vectorize(lambda gray: chr(choice(dict_[gray])))(img)
-string_list = string_array.flatten()
+mode = "static"
+if args.command in {"static", "s"}:
+    mode = "static"
+    count = args.count
+    dict_ = tuple(
+        map(lambda i: i[::-1][:count], load(open(join(cur_dir, "data.json"))))
+    )
+elif args.command in {"mobile", "m"}:
+    mode = "mobile"
+    dict_ = tuple(map(lambda i: i[-1], load(open(join(cur_dir, "data.json")))))
+    fps = args.fps
+    time = args.time
+    t_minimum = args.t_minimum
+    t_maximum = args.t_maximum
+    parametric_equation_x = sp.sympify(args.parametric_equation_x)
+    parametric_equation_y = sp.sympify(args.parametric_equation_y)
 
 font1 = ImageFont.truetype(font_path, char_size)
 font2 = ImageFont.truetype(font_path2, char_size)
 font3 = ImageFont.truetype(font_path3, char_size)
 font4 = ImageFont.truetype(font_path4, char_size)
 
-img = Image.new('RGBA', (size * char_size, height * char_size), bgc)
-draw = ImageDraw.Draw(img)
-for index, char in tqdm(enumerate(string_list), total=height * size):
-    x = index % size
-    y = index // size
-    font = font1
-    if 0x3400 <= ord(char) <= 0x9FFF:
-        font = font1
-    elif 0x20000 <= ord(char) <= 0x2EBE0:
-        font = font2
-    elif 0x30000 <= ord(char) <= 0x323AF:
-        font = font3
-    elif 0x2EBF0 <= ord(char) <= 0x2EE5D:
-        font = font4
-    if args.colorful:
-        color = tuple(img_data[y, x])
-    else:
-        color = (0, 0, 0, 0)
-    draw.text((x * char_size, (y + 1) * char_size), char, color, font, anchor='ls')
+img = Image.open(image_path)
+width, height = img.size
+aspect_ratio = height / width
+img = img.resize((size, (height := round(size * aspect_ratio))))
 
-img.save(output_path)
+if colorful:
+    img_rgba = img.convert("RGBA")
+    img_data = np.array(img_rgba)
+
+if normalize_brightness_target is not None:
+    from normalize_brightness import normalize_brightness
+
+    img = normalize_brightness(img, normalize_brightness_target)
+
+enhancer = ImageEnhance.Contrast(img)
+img = enhancer.enhance(contrast_factor)
+
+if path_save_pretreated is not None:
+    img.save(path_save_pretreated)
+
+if mode == "mobile":
+
+    def get_weighted_average(arr, center, radius=1):
+        arr = arr
+        center_row, center_col = center
+        rows, cols = arr.shape
+        y_indices, x_indices = np.indices((rows, cols))
+        distances = np.sqrt(
+            (y_indices - center_row) ** 2 + (x_indices - center_col) ** 2
+        )
+
+        mask = distances < radius
+        weights = radius - distances[mask]
+        weighted_elements = arr[mask] * weights
+
+        weighted_average = (
+            weighted_elements.sum() / weights.sum() if weights.sum() > 0 else 0
+        )
+        return weighted_average
+
+    def get_weighted_average_3d(arr, center, radius=1):
+        arr = np.array(arr)
+        center_row, center_col = center
+        rows, cols, _ = arr.shape
+
+        y_indices, x_indices = np.indices((rows, cols))
+        distances = np.sqrt(
+            (y_indices - center_row) ** 2 + (x_indices - center_col) ** 2
+        )
+
+        mask = distances < radius
+        weights = radius - distances[mask]
+
+        weighted_sum = np.tensordot(arr[mask], weights, axes=(0, 0))
+        weights_sum = weights.sum()
+        weighted_average = (
+            weighted_sum / weights_sum if weights_sum > 0 else np.zeros(4)
+        )
+
+        return weighted_average
+
+    def draw_text(img_array, offset=(0, 0)):
+        img = Image.new(
+            "RGBA", (size * char_size, height * char_size), background_color
+        )
+        draw = ImageDraw.Draw(img)
+        for yx in tqdm(
+            np.ndindex(tuple([i + 2 for i in img_array.shape])),
+            total=(height + 2) * (size + 2),
+            leave=False,
+        ):
+            y, x = yx
+            y -= 1
+            x -= 1
+            offset_x = x + offset[0]
+            offset_y = y + offset[1]
+            gray_value = get_weighted_average(img___, (offset_y, offset_x), 1)
+            char = chr(dict_[int(gray_value)])
+            if 0x3400 <= ord(char) <= 0x9FFF:
+                font = font1
+            elif 0x20000 <= ord(char) <= 0x2EBE0:
+                font = font2
+            elif 0x30000 <= ord(char) <= 0x323AF:
+                font = font3
+            elif 0x2EBF0 <= ord(char) <= 0x2EE5D:
+                font = font4
+            else:
+                font = font1
+            if colorful:
+                color = get_weighted_average_3d(img_data, (offset_y, offset_x), 1)
+                color = tuple(np.asarray(color, int))
+            else:
+                color = (0, 0, 0, 0)
+            draw.text(
+                (offset_x * char_size, (offset_y + 1) * char_size),
+                char,
+                color,
+                font,
+                anchor="ls",
+            )
+
+        return img
+
+    def calculate_point(t):
+        t_val = t_minimum + (t_maximum - t_minimum) * t
+        x = float(parametric_equation_x.subs("t", t_val))
+        y = float(parametric_equation_y.subs("t", t_val))
+        return np.array([x % 1, y % 1])
+
+    points = np.linspace(0, 1, (fps * time) + 1)
+    offsets_list = list(map(calculate_point, points))
+
+    img___ = np.array(img.convert("L"))
+    imgs = []
+
+    for offset in tqdm(offsets_list[:-1]):
+        imgs.append(draw_text(img___, offset))
+
+    imgs[0].save(
+        output_path,
+        save_all=True,
+        append_images=imgs[1:],
+        optimize=False,
+        duration=int(1000 / fps),
+        loop=0,
+    )
+if mode == "static":
+    if colorful:
+        img_data = np.array(img.convert("RGBA"))
+    img = img.convert("L")
+    string_array = np.vectorize(lambda gray: chr(choice(dict_[gray])))(img)
+    string_list = string_array.flatten()
+
+    img = Image.new("RGBA", (size * char_size, height * char_size), background_color)
+    draw = ImageDraw.Draw(img)
+    for index, char in tqdm(enumerate(string_list), total=height * size):
+        x = index % size
+        y = index // size
+        font = font1
+        if 0x3400 <= ord(char) <= 0x9FFF:
+            font = font1
+        elif 0x20000 <= ord(char) <= 0x2EBE0:
+            font = font2
+        elif 0x30000 <= ord(char) <= 0x323AF:
+            font = font3
+        elif 0x2EBF0 <= ord(char) <= 0x2EE5D:
+            font = font4
+        if args.colorful:
+            color = tuple(img_data[y, x])
+        else:
+            color = (0, 0, 0, 255)
+        draw.text((x * char_size, (y + 1) * char_size), char, color, font, anchor="ls")
+
+    img.save(output_path)
